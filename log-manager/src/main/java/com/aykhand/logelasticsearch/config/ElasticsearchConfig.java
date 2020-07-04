6818d38bc29bd4381ad835adea82a8c5d7bb7272
ayxan386 <ayxanpro@gmail.com>
2020-07-04 20:20:31 +0400

    logs/ms: externilized the elasticsearch configuration; added Dockerfile to logs-ms

--- log-manager/src/main/java/com/aykhand/logelasticsearch/config/ElasticsearchConfig.java
+++ log-manager/src/main/java/com/aykhand/logelasticsearch/config/ElasticsearchConfig.java
@@ -1,9 +1,28 @@
 package com.aykhand.logelasticsearch.config;
 
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories
-public class ElasticsearchConfig {
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
+  @Value("${elasticsearch.host}")
+  private String host;
+  @Value("${elasticsearch.port}")
+  private int port;
+
+  @Override
+  @Bean
+  public RestHighLevelClient elasticsearchClient() {
+    final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+        .connectedTo(String.format("%s:%d", host, port))
+        .build();
+    return RestClients.create(clientConfiguration).rest();
+  }
 }
