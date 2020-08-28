fa65c63c76c59211a648223e1ad93500c31f5c36
ayxan386 <ayxanpro@gmail.com>
2020-08-28 21:22:41 +0400

    scala/dataprivder: added generic response pattern to use for all services

--- data-service/scala-sql-dataprovider/app/dtos/ResponseDTO.scala
+++ data-service/scala-sql-dataprovider/app/dtos/ResponseDTO.scala
@@ -1,17 +1,27 @@
 package dtos
 
 import play.api.libs.json.Json.toJson
-import play.api.libs.json.{JsArray, JsNumber, JsObject, JsString, JsValue, Json, Writes}
+import play.api.libs.json.{
+  JsArray,
+  JsNumber,
+  JsObject,
+  JsString,
+  JsValue,
+  Json,
+  Writes
+}
 
-class ResponseDTO[T](val data: T,val message: String) {}
+class ResponseDTO[T](val data: T, val message: String) {}
 
 object ResponseDTO {
-  implicit def writes[T](implicit fmt: Writes[T]): Writes[ResponseDTO[T]] = new Writes[ResponseDTO[T]] {
-    def writes(ts: ResponseDTO[T]) = JsObject(Seq(
-      "data" -> Json.toJson(ts.data),
-      "message" -> JsString(ts.message)
-    ))
-  }
-
+  implicit def writes[T](implicit fmt: Writes[T]): Writes[ResponseDTO[T]] =
+    new Writes[ResponseDTO[T]] {
+      def writes(ts: ResponseDTO[T]) =
+        JsObject(
+          Seq(
+            "data" -> Json.toJson(ts.data),
+            "message" -> JsString(ts.message)
+          ))
+    }
   def wrappedIn[T](data: T, message: String) = new ResponseDTO(data, message)
 }
